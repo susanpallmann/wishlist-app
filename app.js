d59e036ee701603ca5af2aaefe6125fedd83457d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-11-03 20:33:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -61,52 +61,37 @@ function signUp(email, password) {
 
 /* DOM Interactions */
 
-$('document').ready(function () {
-    
-    // If an element with DOM attribute "function" set to "signout" is clicked
-    $('document').on('click', '#signout', function() {
+// If an element with DOM attribute "function" set to "signout" is clicked
+$(document).on('click', '#signout', function() {
 
-        // Call function signOut to handle authentication sign out
-        signOut();
+    // Call function signOut to handle authentication sign out
+    signOut();
 
-        // Prevent default click behavior
-        event.preventDefault();
-        return false;
-    });
-
-
-    // If an element with DOM attribute "function" set to "login" is clicked
-    $('document').on('click', '#login', function() {
-
-        // Call function logIn to handle authentication log in
-        logIn($('#login-email').val(), $('#login-password'));
-
-        // Prevent default click behavior
-        event.preventDefault();
-        return false;
-    });
-
-    // If an element with ID "signup" is clicked
-    $(document).on('click', '#signup', function() {
-
-        console.log('this ran 1');
-        // Call function logIn to handle authentication log in
-        signUp($('#signup-email').val(), $('#signup-password'));
-
-        // Prevent default click behavior
-        //event.preventDefault();
-        //return false;
-    });
+    // Prevent default click behavior
+    event.preventDefault();
+    return false;
 });
 
+
+// If an element with DOM attribute "function" set to "login" is clicked
+$(document).on('click', '#login', function() {
+
+    // Call function logIn to handle authentication log in
+    logIn($('#login-email').val(), $('#login-password').val());
+
+    // Prevent default click behavior
+    event.preventDefault();
+    return false;
+});
+
+
 // If an element with ID "signup" is clicked
-    $(document).on('click', '#signup', function() {
+$(document).on('click', '#signup', function() {
 
-        console.log('this ran 2');
-        // Call function logIn to handle authentication log in
-        signUp($('#signup-email').val(), $('#signup-password'));
+    // Call function logIn to handle authentication log in
+    signUp($('#signup-email').val(), $('#signup-password').val());
 
-        // Prevent default click behavior
-        //event.preventDefault();
-        //return false;
-    });
+    // Prevent default click behavior
+    event.preventDefault();
+    return false;
+});
