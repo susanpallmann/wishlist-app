74ee440696dc01bd5a66916931356a422b97ce37
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-11-18 19:26:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,53 @@ function signUp(displayName, email, password) {
     });
 }
 
+/* Generate Family Code */
+
+// Recursive function to check if the room code is complete and generate random letters if not
+function generateCode(code) {
+    
+    // If the string isn't yet 4 characters long
+    if (code.length < 4) {
+        
+        // Generate a random number between 0 and 25
+        // Convert this new value to an ascii character (uppercase)
+        let newLetter = Math.floor(Math.random() * 25);
+        newLetter = String.fromCharCode(65 + newLetter);
+        
+        // Add the new value to the existing room code
+        code =  code + newLetter;
+
+        // Run this function again to check if the code is complete now (length of 4)
+        generateCode(code);
+        
+    // If the string is 4 characters
+    } else {
+        
+        // End recursion
+        // Passes the 4-digit code into the verifyRoomCode function
+        verifyCode(code);
+    }
+}
+
+// Function to check if the room key passed into it (key) is already an in-session game in the database
+function verifyCode(code) {
+    
+    // Checks that specific location in the database and takes a snapshot
+    firebase.database().ref('families/' + code).once("value", snapshot => {
+
+        // If the snapshot exists already
+        if (snapshot.exists()) {
+            
+            // Rerun the code generator and try again
+            generateCode('');
+            
+        // If the snapshot doesn't exist, we can set up the family
+        } else {
+            return code;
+        }
+    });
+}
+
 /* DOM-User Interactions */
 
 // Shows destination state and hides other states
@@ -125,6 +172,20 @@ $(document).on('click', '#signup', function() {
 
     // Call function logIn to handle authentication log in
     signUp($('#signup-display-name').val(), $('#signup-email').val(), $('#signup-password').val());
+
+    // Prevent default click behavior
+    event.preventDefault();
+    return false;
+});
+
+// If an element with ID "initialize-create-family" is clicked
+$(document).on('click', '#initialize-create-family', function() {
+
+    // Call function generateCode to generate code for form
+    let code = generateCode('');
+    console.log(code);
+    
+    $('#create-family-code').val(code);
 
     // Prevent default click behavior
     event.preventDefault();
